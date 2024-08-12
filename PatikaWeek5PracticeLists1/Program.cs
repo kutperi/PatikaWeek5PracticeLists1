03c47a419249324950ec366d8cad52db97c21f7e
Bilal <bilalbilin12@gmail.com>
2024-08-12 17:41:18 +0300

    Add project files.

---
+++ PatikaWeek5PracticeLists1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+List<string> guestList = new List<string>() // Creating the list consist guest names
+{
+    "Bülent Ersoy",
+    "Ajda Pekkan",
+    "Ebru Gündeş",
+    "Hadise",
+    "Hande Yener",
+    "Tarkan",
+    "Funda Arar",
+    "Demet Akalın"
+};
+
+Console.WriteLine("-------**Davetliler**------");
+
+foreach (string guest in guestList) // Printing the elements of the list
+{
+    Console.WriteLine(guest);
+}
